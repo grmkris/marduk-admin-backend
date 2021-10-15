659746cbfcd1ac2c90b7b802c8ec564a72ba64eb
kristjangrm <kristjan.grm1@gmail.com>
2021-10-15 18:31:34 +0200

    inital commit

---
+++ src/main/java/com/grmkris/btcrbtcswapper/BtcRbtcSwapperApplication.java
@@ -0,0 +1,13 @@
+package com.grmkris.btcrbtcswapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcRbtcSwapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtcRbtcSwapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grmkris/btcrbtcswapper/BtcRbtcSwapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grmkris.btcrbtcswapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcRbtcSwapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
